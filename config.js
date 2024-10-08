6bd42298ef18c2cbcee70c72bde6c8ee3fe3b3ab
ᴍʟ ᴀʀᴍʏ <128611730+mlarmy@users.noreply.github.com>
2024-10-08 16:27:48 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const name = "SuperShop";
+const firebaseConfig = {
+    apiKey: "AIzaSyDXl_4DpPjg_W-Kxly8dFFFwb53irmOntE",
+    authDomain: "supershop-26b35.firebaseapp.com",
+    databaseURL: "https://supershop-26b35-default-rtdb.firebaseio.com",
+    projectId: "supershop-26b35",
+    storageBucket: "supershop-26b35.appspot.com",
+    messagingSenderId: "848422254720",
+    appId: "1:848422254720:web:c1dfced4d1525d2e7cce65",
+    measurementId: "G-576GF34SFV"
+};
